2cb5369677f1e898a52b4ce2f64538d1a112a584
Nimesh Kumarasinghe <nimesh>
2024-10-27 12:06:53 +0530

    Splashscreen development with temp next page

---
+++ PlanMate/PlanMate/SplashScreenView.swift
@@ -0,0 +1,58 @@
+//
+//  SplashScreenView.swift
+//  PlanMate
+//
+//  Created by COBSCCOMPY4231P-005 on 2024-10-27.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    // check splash screen active
+    @State private var isActive: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                if isActive {
+                    // afters plash screen navigate to onboarding
+                   Next()
+                    
+                } else {
+                    VStack {
+                        Spacer()
+                        Image("PlanMate")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 200, height: 200)
+                        
+                        Spacer()
+                    }
+                    .onAppear {
+                        // timmer for splashscreen
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                            withAnimation {
+                                self.isActive = true
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .navigationBarHidden(true) // Hide navigation bar during splash screen
+    }
+}
+
+// Temporary next page
+struct Next: View {
+    var body: some View {
+        Text("Welcome to PlanMate!")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
+
