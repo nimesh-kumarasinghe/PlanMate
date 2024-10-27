e6bc18162ebec49b5c63fb3fded84ca88477896e
Nimesh Kumarasinghe <nimesh>
2024-10-27 11:51:45 +0530

    Initial Commit

---
+++ PlanMate/PlanMate/PlanMateApp.swift
@@ -0,0 +1,20 @@
+//
+//  PlanMateApp.swift
+//  PlanMate
+//
+//  Created by COBSCCOMPY4231P-005 on 2024-10-27.
+//
+
+import SwiftUI
+
+@main
+struct PlanMateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
